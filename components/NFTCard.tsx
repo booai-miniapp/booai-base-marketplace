608543b376e237216e1c33e7e30e10c4225c448d
booai-miniapp <hamzamuhammad32212@gmail.com>
2025-11-14 19:09:23 +0100

    Update NFTCard.tsx

--- components/NFTCard.tsx
+++ components/NFTCard.tsx
@@ -1,1 +1,32 @@
+export default function NFTCard({ nft }) {
+  return (
+    <div
+      style={{
+        border: "1px solid #333",
+        borderRadius: 12,
+        padding: 15,
+        marginBottom: 20
+      }}
+    >
+      <img
+        src={nft.image}
+        alt={nft.name}
+        style={{ width: "100%", borderRadius: 12 }}
+      />
 
+      <h3>{nft.name}</h3>
+      <p>{nft.collection}</p>
+
+      <button
+        style={{
+          background: "#0ff",
+          padding: "10px 20px",
+          borderRadius: 10,
+          border: "none"
+        }}
+      >
+        Buy
+      </button>
+    </div>
+  );
+}
