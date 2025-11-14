2f8823f0895a618e9f39b2156f4e7d96a0f99741
booai-miniapp <hamzamuhammad32212@gmail.com>
2025-11-14 19:05:05 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,1 +1,16 @@
+"use client";
 
+import Marketplace from "../components/Marketplace";
+import ConnectWallet from "../components/ConnectWallet";
+
+export default function Home() {
+  return (
+    <main style={{ padding: 20 }}>
+      <h1>BooAI Base Marketplace</h1>
+
+      <ConnectWallet />
+
+      <Marketplace />
+    </main>
+  );
+}
